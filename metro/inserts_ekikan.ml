d34779d4f422888718103f84d1cb35de0ddc29a8
snufkon <snufkon@gmail.com>
2018-01-08 07:30:40 +0900

    fix a bit

--- metro/inserts_ekikan.ml
+++ metro/inserts_ekikan.ml
@@ -1,9 +1,9 @@
 (* 前提: ekikan_t, ekikan_tree_t, inserts_ekikan ロード済み *)
-
+p
 (* 17.13 *)
 (* 目的: ekikan_tree_t 型の木と ekikan_t list 型の駅間リストを受け取り
    リストに含まれる駅間をすべて挿入した木を返す *)
+(* inserts_ekikan: ekikan_tree_t -> ekikan_t list -> ekikan_tree_t *)
 let rec inserts_ekikan tree ekikan_list =
-  List.fold_left insert_ekikan Empty ekikan_list
+  List.fold_left insert_ekikan tree ekikan_list
 ;;
-
